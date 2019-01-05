dc389755bc04a977109fa8c4638854110ad27c96
Esten Rye <esten@ryezone.net>
2019-01-05 15:58:06 -0600

    Working Hyper-V builds for minimal server and netboot desktop.

--- build.ps1
+++ build.ps1
@@ -1,1 +1,12 @@
-&"$PSScriptRoot/bin/packer" build -only=hyperv-iso -force $PSScriptRoot/packer_templates/ubuntu1804.json
+param(
+    [ValidateSet('18.04')]
+    [string]$version = '18.04',
+    
+    [ValidateSet('ubuntu-desktop-netboot', 'ubuntu-server-minimal')]
+    [string]$varfile = 'ubuntu-desktop-netboot'
+)
+&"$PSScriptRoot/bin/packer" build `
+    -only=hyperv-iso `
+    -force `
+    -var-file="$PSScriptRoot/packer_templates/$version/$($varfile).json" `
+    $PSScriptRoot/packer_templates/$version/ubuntu.json
