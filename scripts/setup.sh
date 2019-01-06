ad5c211d49860bb5be5d630c2028d29d9f250f53
Esten Rye <esten.rye@ryezone.com>
2019-01-06 09:29:13 -0600

    move cloud token env var out of var files. add vagrant insecure ssh key.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,8 @@
 # Add vagrant user to sudoers.
 echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+mkdir -p /home/vagrant/.ssh
+curl https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant > /home/vagrant/.ssh/id_rsa
 
 # Disable daily apt unattended updates.
 echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
