c87728e52ef51bc7e5f815d39db15bb9583c55aa
Esten Rye <esten.rye@ryezone.com>
2019-01-10 21:53:50 -0600

    Create bin directory.

--- build.ps1
+++ build.ps1
@@ -8,6 +8,11 @@ param(
     [switch]$publish = $false
 )
 
+if (-not (Test-Path "$PSScriptRoot/bin"))
+{
+    New-Item -ItemType Directory "$PSScriptRoot/bin"
+}
+
 if (-not (Test-Path "$PSScriptRoot/bin/packer.exe"))
 {
     Invoke-WebRequest -UseBasicParsing -Uri https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_windows_amd64.zip -OutFile "$PSScriptRoot/bin/packer.zip"
@@ -20,6 +25,8 @@ if (-not (Test-Path "$PSScriptRoot/bin/jq.exe"))
     Invoke-WebRequest -UseBasicParsing -Uri https://github.com/stedolan/jq/releases/download/jq-1.6/jq-win64.exe -OutFile "$PSScriptRoot/bin/jq.exe"
     Unblock-File "$PSScriptRoot/bin/jq.exe"
 }
+
+pip show awscli
 
 if ($publish)
 {
