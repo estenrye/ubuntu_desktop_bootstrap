fb2f5da50ac301baaad4f4274af5836f3734cf38
Esten Rye <esten.rye@ryezone.com>
2019-01-11 21:46:32 -0600

    Fix the profile access keys

--- setup/install-awscli.sh
+++ setup/install-awscli.sh
@@ -5,3 +5,6 @@ AWSCLI="$(pip show awscli | grep Location | sed 's/Location: //')/awscli"
 python $AWSCLI configure set region us-east-1 --profile wasabi
 python $AWSCLI configure set output json --profile wasabi
 python $AWSCLI configure set s3.endpoint_url https://s3.wasabisys.com --profile wasabi
+python $AWSCLI configure set aws_access_key_id $AWS_ACCESS_KEY --profile wasabi
+python $AWSCLI configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile wasabi
+
