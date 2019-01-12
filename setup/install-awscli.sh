789c770b4bd624425008e135cb607c9b3abc4c7b
Esten Rye <esten.rye@ryezone.com>
2019-01-11 20:28:21 -0600

    Add aws config.

--- setup/install-awscli.sh
+++ setup/install-awscli.sh
@@ -1,1 +1,7 @@
 pip install awscli --upgrade --user
+
+AWSCLI="$(pip show awscli | grep Location | sed 's/Location: //')/awscli"
+
+python $AWSCLI configure set --profile wasabi region=us-east-1
+python $AWSCLI configure set --profile wasabi output=json
+python $AWSCLI configure set --profile wasabi s3.endpoint_url=https://s3.wasabisys.com
