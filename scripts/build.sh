100b9a333522e408eb8be54968a77163231c44fa
Esten Rye <esten.rye@ryezone.com>
2019-01-16 06:49:47 -0600

    make packer one line.

--- scripts/build.sh
+++ scripts/build.sh
@@ -42,15 +42,11 @@ https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_windows_amd64.zip
 
 if [ ! -f "$PACKER" ]; then
     if [ "$(uname -s)" != 'Linux' ]; then
-        curl -L https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_windows_amd64.zip -o "$PACKER_ARCHIVE"
+        $CURL -L https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_windows_amd64.zip -o "$PACKER_ARCHIVE"
     else
-        curl -L https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_linux_amd64.zip -o "$PACKER_ARCHIVE"
+        $CURL -L https://releases.hashicorp.com/packer/1.3.3/packer_1.3.3_linux_amd64.zip -o "$PACKER_ARCHIVE"
     fi
     unzip -f $PACKER_ARCHIVE -d "${BIN}/bin"
 fi
 
-$PACKER build \
-    -only=$VAGRANT_BUILDER_TYPE \
-    -force \
-    -var-file="${BIN}/packer_templates/${UBUNTU_VERSION}/$($VAR_FILE).json" \
-    ${BIN}/packer_templates/${UBUNTU_VERSION}/ubuntu.json
+$PACKER build -only=$VAGRANT_BUILDER_TYPE -force -var-file="${BIN}/packer_templates/${UBUNTU_VERSION}/$($VAR_FILE).json" ${BIN}/packer_templates/${UBUNTU_VERSION}/ubuntu.json
