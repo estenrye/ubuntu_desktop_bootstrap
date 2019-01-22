4a9582855eae3f7f11fa6d7f991671fe698d7002
Esten Rye <esten.rye@ryezone.com>
2019-01-21 21:39:30 -0600

    add parameters to set environment vars.

--- setup/install-awscli.sh
+++ setup/install-awscli.sh
@@ -1,3 +1,29 @@
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -a|--awsAccessKey)
+    AWS_ACCESS_KEY="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -s|--awsSecretKey)
+    AWS_SECRET_ACCESS_KEY="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+
+
 pip install awscli --upgrade --user
 
 AWSCLI="$(pip show awscli | grep Location | sed 's/Location: //')/awscli"
