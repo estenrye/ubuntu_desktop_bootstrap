5e627e27efc6e3108dddcc30151144bbecab9145
Esten Rye <esten.rye@ryezone.com>
2019-02-09 12:25:53 -0600

    Fix publish version.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -53,4 +53,4 @@ echo "https://app.vagrantup.com/api/v1/box/$VAGRANT_CLOUD_USERNAME/$VAGRANT_BOX_
 
 echo "https://app.vagrantup.com/api/v1/box/$VAGRANT_CLOUD_USERNAME/$VAGRANT_BOX_NAME/version/$VAGRANT_BOX_VERSION/providers"
 # Create a new provider
-"$CURL" --header "Content-Type: application/json" --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" https://app.vagrantup.com/api/v1/box/$VAGRANT_CLOUD_USERNAME/$VAGRANT_BOX_NAME/version/$VAGRANT_BOX_VERSION/providers --data "{ \"provider\": { \"name\": \"$VAGRANT_PROVIDER_TYPE\", \"url\":\"https://s3.wasabisys.com/vagrant-cloud/$VAGRANT_BOX_NAME/$VAGRANT_PROVIDER_TYPE-$BUILD_BUILDNUMBER.box\" } }"
+"$CURL" --header "Content-Type: application/json" --header "Authorization: Bearer $VAGRANT_CLOUD_TOKEN" https://app.vagrantup.com/api/v1/box/$VAGRANT_CLOUD_USERNAME/$VAGRANT_BOX_NAME/version/$VAGRANT_BOX_VERSION/providers --data "{ \"provider\": { \"name\": \"$VAGRANT_PROVIDER_TYPE\", \"url\":\"https://s3.wasabisys.com/vagrant-cloud/$VAGRANT_BOX_NAME/$VAGRANT_PROVIDER_TYPE-$VAGRANT_BOX_VERSION.box\" } }"
